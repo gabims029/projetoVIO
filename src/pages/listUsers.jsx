afa71c48d3ccbbd5947e0f21ee8d503f8ce91139
gabims029 <gabimelo541@gmail.com>
2025-04-16 16:07:31 -0300

    Função delete

--- src/pages/listUsers.jsx
+++ src/pages/listUsers.jsx
@@ -10,12 +10,36 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import Paper from '@mui/material/Paper';
 import api from '../axios/axios'
-import { Button } from '@mui/material';
+import { Button, IconButton, Alert, Snackbar } from '@mui/material';
 import { Link , useNavigate } from 'react-router-dom';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function listUsers() {
   const [users,setUsers] = useState([]);
+  const [alert, setAlert] = useState({
+    //Visibilidade (false = oculta; true = visível )
+    open: false,
+
+    //Nível do alerta (sucess, error, warning, etc)
+    severity:"",
+
+    //Mensagem que será exibida
+    message:""
+  });
+
+  //Função para exibir o alerta 
+  const showAlert = (severity, message) => {
+    setAlert({open: true, severity, message})
+  };
+
+  //fechar o alerta
+  const handleCloseAlert = () => {
+    setAlert({...alert, open:false})
+  };
+
   const navigate = useNavigate();
+
   async function getUsers(){
     // Chamada da Api
     await api.getUsers().then(
@@ -28,12 +52,32 @@ function listUsers() {
     )
   }
 
+  async function deleteUser(id){
+    try{
+      await api.deleteUser(id);
+      await getUsers();
+      showAlert("success", "Usuário excluído com sucesso!")
+    }catch(error){
+      console.log("Erro ao deletar usuário...", error);
+      showAlert("error", error.response.data.error);
+
+    }
+  }
+
   const listUsers = users.map((user)=>{
     return(
       <TableRow key={user.id_usuario}>
         <TableCell align="center">{user.name}</TableCell>
         <TableCell align="center">{user.email}</TableCell>
         <TableCell align="center">{user.cpf}</TableCell>
+
+        <TableCell align="center">
+          <IconButton onClick={() => deleteUser(user.id)}>
+            <DeleteOutlineIcon color="error"/>
+          </IconButton>
+        </TableCell>
+
+
       </TableRow>
     )
   });
@@ -53,6 +97,15 @@ function listUsers() {
 
   return (
     <div>
+      <Snackbar open={alert.open} autoHideDuration={3000} onClose={handleCloseAlert}
+      anchorOrigin={{vertical:"top", horizontal:"center"}}>
+        <Alert onClose={handleCloseAlert}
+        severity={alert.severity} 
+        sx={{width:"100%"}}>
+          {alert.message}
+        </Alert>
+
+      </Snackbar>
       {users.length === 0 ?(<h1>Carregando usuários</h1>): (
       <div>
         <h5>Lista de usuários</h5>
@@ -68,6 +121,9 @@ function listUsers() {
                 </TableCell>
                 <TableCell align="center">
                   CPF
+                </TableCell>
+                <TableCell align="center">
+                  Ações
                 </TableCell>
               </TableRow>
             </TableHead>
